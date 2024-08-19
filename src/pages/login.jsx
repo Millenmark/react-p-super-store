16d37a5e5891bbc41f5f6d97f508e482fc9dd0f3
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-08-19 20:39:26 +0800

    fixed login and register helmet

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
   return (
     <>
       <Helmet>
-        <title> Login | Minimal UI </title>
+        <title> Login | Super Store </title>
       </Helmet>
 
       <LoginView />
